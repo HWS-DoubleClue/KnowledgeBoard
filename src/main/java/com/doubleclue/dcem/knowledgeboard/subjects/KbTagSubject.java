27f437095d995dfbe4bab94ee9b1a0207d1d4740
Maximilian Schmidt <maximilian.schmidt@hws-gruppe.de>
2024-01-22 15:39:28 +0100

    role:helpdesk has view rights for tags

--- src/main/java/com/doubleclue/dcem/knowledgeboard/subjects/KbTagSubject.java
+++ src/main/java/com/doubleclue/dcem/knowledgeboard/subjects/KbTagSubject.java
@@ -42,7 +42,7 @@ public class KbTagSubject extends SubjectAbs {
 		rawActionQuestion.setIcon("far fa-question-circle");
 		rawActions.add(rawActionQuestion);
 		
-		rawActions.add(new RawAction(DcemConstants.ACTION_VIEW, new String[] { DcemConstants.SYSTEM_ROLE_USER}));
+		rawActions.add(new RawAction(DcemConstants.ACTION_VIEW, new String[] { DcemConstants.SYSTEM_ROLE_USER, DcemConstants.SYSTEM_ROLE_HELPDESK, DcemConstants.SYSTEM_ROLE_VIEWER}));
 		rawActions.add(new RawAction(DcemConstants.ACTION_MANAGE, new String[] { DcemConstants.SYSTEM_ROLE_SUPERADMIN, DcemConstants.SYSTEM_ROLE_ADMIN }));
 	};
 
